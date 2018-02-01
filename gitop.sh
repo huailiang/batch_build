1e484e8278a07c131e7b0125745809aad610ea0c
alexpeng <alexpeng@123u.com>
2018-02-01 20:05:36 +0800

    合线optms

--- gitop.sh
+++ gitop.sh
@@ -15,87 +15,44 @@ UNITY_PATH=/Applications/Unity/Unity5.app/Contents/MacOS/Unity
 
 ################ res ###################
 
-echo "正在准备开始。。。"
+echo "开始清理本地环境，拉取最新，合线"
 
 #全局配置
 git config --global credential.helper store
 git config --global merge.ours.driver true
 
-cd ${PROJECT_RES}
+function gitoption() { 
+	git clean -dfq
+	git reset --hard HEAD^ 
+	echo "** switch branch ob **"
+	git checkout OB
+	git pull -q
+	echo "** switch branch release **"
+	git checkout $1
+	git pull -q
+	git log -1
+	git merge OB -m "dn-version shell merge from OB"
+	if [ $? -ne 0 ];then
+	    echo "error merge src"
+	  	exit 1
+	fi
+	git push
+	echo "\n"
+}
 
-git reset --hard | git clean -dfq
+#下面两行是正式版本用 先注释掉
+#cd ${PROJECT_RES} && echo "********** res *********" && gitoption release-res
+#cd ${PROJECT_SRC} && echo "********** src *********" && gitoption release-src
 
-#echo "checkout res ob"
+#测试代码
+cd ${PROJECT_RES} && git clean -dfq && git reset --hard && git checkout release_jenkins && git pull
 
-# git checkout OB
-
-git pull -q
-
-git log -1
-
-# ehco "checkout res release"
-
-# git checkout release-res
-
-# git pull
-
-# git merge origin/OB -m "shell merge from OB"
-
-# if [ $? -ne 0 ];then
-#     echo "error merge src"
-#     exit 1
-# fi
-
-# #git push
-
-# echo "merge res finish\n\n"
-
-
-# ################ src ###################
-
-# echo ${PROJECT_SRC}
-
-# cd $PROJECT_SRC/
-
-# git reset --hard | git clean -dfq
-
-# echo "checkout res ob"
-
-# git checkout OB
-
-# git pull
-
-#git log -1
-
-# ehco "checkout res release"
-
-# git checkout release-src
-
-# git pull
-
-# git merge origin/OB -m "shell merge from OB"
-
-# if [ $? -ne 0 ];then
-#     echo "error merge src"
-#     exit 1
-# fi
-
-# #git push
-
-# echo "merge src finish"
-
+echo "git 操作结束"
 
 echo "开始统计文件变更列表，请耐心等待..."
 
-macro="Publish"
-
-echo $nmacro>${PROJECT_RES}"/XProject/Assets/Build.txt"
-
-#$UNITY_PATH -projectPath ${PROJECT_RES}"/XProject"  -logFile /tmp/dn_patch_setmacro.log -executeMethod SelectPlatformEditor.SetMacrow -quit -batchmode
-
 #call unity interface 
 $UNITY_PATH -quit -batchmode -projectPath ${PROJECT_RES}"/XProject" -logFile /tmp/dn_patch_patch.log -executeMethod XBundlePresent.JenkinsFetchList
-
 
 if [ -f "${PROJECT_RES}/XProject/Shell/Error.Log" ];then
 	echo "error occur, as list:"
